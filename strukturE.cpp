2dcff8fe7ea4a937c8fa82423aa4bd0d02cabced
Cla1reee <h.harimurti.ft23@mail.umy.ac.id>
2024-04-02 16:15:14 +0700

    membuat input data mahasiswa

--- strukturE.cpp
+++ strukturE.cpp
@@ -9,4 +9,13 @@ struct Mahasiswa{
 };
 
 int main() {
+        Mahasiswa mhs;
+    cout << "nomor mahasiswa : ";
+    cin >> mhs.nim;
+    cout << "nama mahasiswa : ";
+    cin >> mhs.nama;
+    cout << "alamat mahasiswa : ";
+    cin >> mhs.alamat;
+    cout << "umur mahasiswa : ";
+    cin >> mhs.umur;
 }
