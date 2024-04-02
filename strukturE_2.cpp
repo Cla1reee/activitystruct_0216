2bcd173fc0e4bd4a38b874cd02ba04154549cda8
Cla1reee <h.harimurti.ft23@mail.umy.ac.id>
2024-04-02 16:52:05 +0700

    menambahkan detail alamat pada input

--- strukturE_2.cpp
+++ strukturE_2.cpp
@@ -13,17 +13,22 @@ struct Mahasiswa{
     string umur;
 };
 
-
-int main(){
- Mahasiswa mhs;
+int main() {
+        Mahasiswa mhs;
     cout << "nomor mahasiswa : ";
     cin >> mhs.nim;
     cout << "nama mahasiswa : ";
     cin >> mhs.nama;
+
     cout << "alamat mahasiswa : ";
-    cin >> mhs.alamat;
+    cout << "\t nama desa : ";
+    cin >> mhs.alamat.desa;
+    cout << "\t nama kota : ";
+    cin >> mhs.alamat.kota;
+
     cout << "umur mahasiswa : ";
     cin >> mhs.umur;
+
     cout << endl;
     cout << "\n nim : " << mhs.nim;
     cout << "\n nama : " << mhs.nama;
